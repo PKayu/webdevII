3ec2d635b54da8b73b0016366d96105dc9b75d09
Dan Worwood <danworwood@gmail.com>
2020-11-04 17:00:26 -0700

    Add project files

---
+++ project/scripts/home.js
@@ -0,0 +1,15 @@
+﻿function clickMenu() {
+	var menu = document.getElementById("menu");
+	var sidemenu = document.getElementById("sidemenu");
+
+	if(menu.className === "open") {
+		menu.className = "close";
+        sidemenu.style.width = "0em";
+        sidemenu.style.minWidth = "0em";
+		return;
+	}
+
+	menu.className = "open";
+    sidemenu.style.width = "25%";
+    sidemenu.style.minWidth = "10em";
+}
